545b2bc97858582a22cebddd4ef0fe66bb425141
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:54:27 +0530

    Initialize greenkeeper/monorepo.apollo-tooling-20200213192639

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const INDEX = '/';
+export const SIGN_IN = '/sign-in';
+export const SIGN_UP = '/sign-up';
+export const ACCOUNT = '/account';
+export const PASSWORD_CHANGE = '/password-change';
+export const PASSWORD_FORGOT = '/password-forgot';
+export const REFERRAL = '/referral';
