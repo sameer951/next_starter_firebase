0155eea82d35bab6e2e5840e8f3211b15286325f
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:54:03 +0530

    Initialize greenkeeper/stripe-pin-8.12.0

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['./.next/', './node_modules/'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@api(.*)$': '<rootDir>/src/api$1',
+    '^@screens(.*)$': '<rootDir>/src/screens$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^@services(.*)$': '<rootDir>/src/services$1',
+    '^@constants(.*)$': '<rootDir>/src/constants$1',
+    '^@context(.*)$': '<rootDir>/src/context$1',
+    '^@queries(.*)$': '<rootDir>/src/queries$1',
+    '^@typeDefs(.*)$': '<rootDir>/src/types$1',
+    '^@generated(.*)$': '<rootDir>/src/generated$1',
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  clearMocks: true,
+};
