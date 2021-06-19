5c1a19acb7419616f71ffaa78fa47bfcad7d5b99
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:28 +0530

    Initialize greenkeeper/@sentry/browser-5.15.1

---
+++ src/services/coupon/spec.ts
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+import { getAsDiscount } from './';
+
+describe('getAsDiscount', () => {
+  it('applies successfully the discount if coupon', async () => {
+    const data = {
+      data: {
+        ppp: {
+          pppConversionFactor: 0.1,
+        },
+      },
+    };
+
+    const get = jest.spyOn(axios, 'get');
+    get.mockImplementationOnce(() => Promise.resolve(data));
+
+    await expect(
+      getAsDiscount(
+        'THE_ROAD_TO_LEARN_REACT',
+        'PROFESSIONAL',
+        100,
+        'MY_COUPON',
+        ''
+      )
+    ).resolves.toEqual(10);
+  });
+
+  it('applies no discount if no coupon', async () => {
+    const data = {
+      data: {
+        ppp: {
+          pppConversionFactor: 0.1,
+        },
+      },
+    };
+
+    const get = jest.spyOn(axios, 'get');
+    get.mockImplementationOnce(() => Promise.resolve(data));
+
+    await expect(
+      getAsDiscount(
+        'THE_ROAD_TO_LEARN_REACT',
+        'PROFESSIONAL',
+        100,
+        null,
+        ''
+      )
+    ).resolves.toEqual(100);
+  });
+
+  it('applies no discount if third-party API throws error', async () => {
+    const get = jest.spyOn(axios, 'get');
+    get.mockImplementationOnce(() =>
+      Promise.reject(new Error('error'))
+    );
+
+    await expect(
+      getAsDiscount(
+        'THE_ROAD_TO_LEARN_REACT',
+        'PROFESSIONAL',
+        100,
+        'MY_COUPON',
+        ''
+      )
+    ).resolves.toEqual(100);
+  });
+});
