69deeb63a941deb46d0b0f48cff4ebe31c3f880b
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:40 +0530

    Initialize greenkeeper/@sentry/browser-5.15.4

---
+++ src/api/authorization/index.ts
@@ -0,0 +1,21 @@
+import { shield, and } from 'graphql-shield';
+
+import { isAuthenticated } from './isAuthenticated';
+import { isAdmin } from './isAdmin';
+import { isFreeCourse } from './isFreeCourse';
+
+export default shield({
+  Query: {
+    me: isAuthenticated,
+
+    discountedPrice: isAuthenticated,
+  },
+  Mutation: {
+    migrate: and(isAuthenticated, isAdmin),
+
+    passwordChange: isAuthenticated,
+
+    createFreeCourse: and(isAuthenticated, isFreeCourse),
+    createAdminCourse: and(isAuthenticated, isAdmin),
+  },
+});
