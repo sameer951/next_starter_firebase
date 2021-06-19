52a5a8890b4c932bf1dcdb2df5371acc3d0c3fb7
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:12 +0530

    Initialize greenkeeper/firebase-7.12.0

---
+++ src/api/typeDefs/index.ts
@@ -0,0 +1,49 @@
+import { gql } from 'apollo-server-express';
+
+import sessionSchema from './session';
+import userSchema from './user';
+import storefrontSchema from './storefront';
+import paypalSchema from './paypal';
+import stripeSchema from './stripe';
+import courseSchema from './course';
+import bookSchema from './book';
+import upgradeSchema from './upgrade';
+
+const linkSchema = gql`
+  enum CourseId {
+    THE_ROAD_TO_LEARN_REACT
+    TAMING_THE_STATE
+    THE_ROAD_TO_GRAPHQL
+    THE_ROAD_TO_REACT_WITH_FIREBASE
+  }
+
+  enum BundleId {
+    STUDENT
+    INTERMEDIATE
+    PROFESSIONAL
+  }
+
+  type Query {
+    _: Boolean
+  }
+
+  type Mutation {
+    _: Boolean
+  }
+
+  type Subscription {
+    _: Boolean
+  }
+`;
+
+export default [
+  linkSchema,
+  sessionSchema,
+  userSchema,
+  storefrontSchema,
+  paypalSchema,
+  stripeSchema,
+  courseSchema,
+  bookSchema,
+  upgradeSchema,
+];
