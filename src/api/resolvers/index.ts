3d5535c30d10aaa25cc0065dbd6b03246aceaf27
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:52 +0530

    Initialize greenkeeper/@types/node-13.9.8

---
+++ src/api/resolvers/index.ts
@@ -0,0 +1,23 @@
+import { resolvers as migrationResolvers } from './migration';
+import { resolvers as sessionResolvers } from './session';
+import { resolvers as userResolvers } from './user';
+import { resolvers as storefrontResolvers } from './storefront';
+import { resolvers as paypalResolvers } from './paypal';
+import { resolvers as stripeResolvers } from './stripe';
+import { resolvers as courseResolvers } from './course';
+import { resolvers as bookResolvers } from './book';
+import { resolvers as upgradeResolvers } from './upgrade';
+import { resolvers as couponResolvers } from './coupon';
+
+export default [
+  migrationResolvers,
+  sessionResolvers,
+  userResolvers,
+  storefrontResolvers,
+  paypalResolvers,
+  stripeResolvers,
+  courseResolvers,
+  bookResolvers,
+  upgradeResolvers,
+  couponResolvers,
+];
