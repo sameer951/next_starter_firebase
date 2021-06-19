1337c8387554c8274bddf91d4e0560a58658eb00
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:55:33 +0530

    Initialize greenkeeper/monorepo.apollo-tooling-20200305190146

---
+++ src/services/firebase/course.ts
@@ -0,0 +1,61 @@
+import * as firebaseAdminVanilla from 'firebase-admin';
+
+import firebaseAdmin from '@services/firebase/admin';
+
+import { COURSE } from '@data/course-keys';
+import { BUNDLE } from '@data/bundle-keys';
+
+export const createCourse = async ({
+  uid,
+  courseId,
+  bundleId,
+  amount,
+  paymentType,
+}: {
+  uid?: string;
+  courseId: COURSE;
+  bundleId: BUNDLE;
+  amount: number;
+  paymentType: string;
+}) =>
+  await firebaseAdmin
+    .database()
+    .ref(`users/${uid}/courses`)
+    .push()
+    .set({
+      courseId: courseId,
+      packageId: bundleId,
+      invoice: {
+        createdAt:
+          firebaseAdminVanilla.database.ServerValue.TIMESTAMP,
+        amount,
+        licensesCount: 1,
+        currency: 'USD',
+        paymentType,
+      },
+    });
+
+export type FirebaseCourseContent = {
+  courseId: COURSE;
+  packageId: BUNDLE;
+  invoice: {
+    amount: number;
+    createdAt: number;
+    currency: string;
+    licencesCount: number;
+    paymentType: string;
+  };
+};
+
+export type FirebaseCourse = {
+  [key: string]: FirebaseCourseContent;
+};
+
+export const getCoursesById = async (
+  uid?: string
+): Promise<FirebaseCourse> =>
+  await firebaseAdmin
+    .database()
+    .ref(`users/${uid}/courses`)
+    .once('value')
+    .then(snapshot => snapshot.val());
