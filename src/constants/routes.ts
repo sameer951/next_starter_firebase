e011db3f26baef96b6cabcc0762c02d3e5c6a976
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:14 +0530

    Initialize feat/apply-coupon

---
+++ src/constants/routes.ts
@@ -0,0 +1,13 @@
+export const SIGN_IN = '/sign-in';
+export const SIGN_UP = '/sign-up';
+export const ACCOUNT = '/account';
+export const PASSWORD_CHANGE = '/password-change';
+export const PASSWORD_FORGOT = '/password-forgot';
+export const REFERRAL = '/referral';
+
+export const INDEX = '/';
+export const UNLOCKED_COURSE_DETAILS = '/p/[unlocked-course-id]';
+
+export const COURSE_UPGRADE = '/upgrade/[upgradeable-course-id]';
+
+export const CHECKOUT = '/checkout';
