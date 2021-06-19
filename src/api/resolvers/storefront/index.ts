32541c213dec26ec5512d16b333de8e7a3c3ee3c
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:31 +0530

    Initialize greenkeeper/stripe-8.37.0

---
+++ src/api/resolvers/storefront/index.ts
@@ -0,0 +1,44 @@
+import sortBy from 'lodash.sortby';
+import { QueryResolvers } from '@generated/server';
+
+import storefront from '@data/course-storefront';
+
+interface Resolvers {
+  Query: QueryResolvers;
+}
+
+export const resolvers: Resolvers = {
+  Query: {
+    storefrontCourse: (parent, { courseId, bundleId }) => {
+      const course = storefront[courseId];
+      const bundle = course.bundles[bundleId];
+
+      return {
+        ...course,
+        header: course.header,
+        courseId: course.courseId,
+        url: course.url,
+        imageUrl: course.imageUrl,
+        canUpgrade: false,
+        bundle,
+      };
+    },
+    storefrontCourses: () => {
+      return Object.values(storefront).map(storefrontCourse => ({
+        courseId: storefrontCourse.courseId,
+        header: storefrontCourse.header,
+        url: storefrontCourse.url,
+        imageUrl: storefrontCourse.imageUrl,
+        canUpgrade: false,
+      }));
+    },
+    storefrontBundles: (parent, { courseId }) => {
+      const course = storefront[courseId];
+
+      return sortBy(
+        Object.values(course.bundles),
+        (bundle: any) => bundle.weight
+      );
+    },
+  },
+};
