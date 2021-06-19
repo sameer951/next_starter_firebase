199127fde31b1d35b2f9ad99105bdd926dfb55b5
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:58:22 +0530

    Initialize greenkeeper/monorepo.apollo-tooling-20200410212004

---
+++ src/api/resolvers/coupon/index.ts
@@ -0,0 +1,50 @@
+import { QueryResolvers, MutationResolvers } from '@generated/server';
+import { priceWithDiscount } from '@services/discount';
+import storefront from '@data/course-storefront';
+
+interface Resolvers {
+  Query: QueryResolvers;
+  Mutation: MutationResolvers;
+}
+
+export const resolvers: Resolvers = {
+  Query: {
+    discountedPrice: async (
+      _,
+      { courseId, bundleId, coupon },
+      { me, courseConnector, couponConnector }
+    ) => {
+      const course = storefront[courseId];
+      const bundle = course.bundles[bundleId];
+
+      if (!me) {
+        return bundle.price;
+      }
+
+      const price = await priceWithDiscount(
+        couponConnector,
+        courseConnector
+      )(courseId, bundleId, bundle.price, coupon, me.uid);
+
+      return {
+        price,
+        isDiscount: price !== bundle.price,
+      };
+    },
+  },
+  Mutation: {
+    couponCreate: async (
+      _,
+      { coupon, discount, count },
+      { couponConnector }
+    ) => {
+      try {
+        await couponConnector.createCoupons(coupon, discount, count);
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      return true;
+    },
+  },
+};
