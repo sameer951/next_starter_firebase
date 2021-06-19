2dac042c19ba457e8e8271b1555ad64bc8f5a089
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:57:56 +0530

    Initialize greenkeeper/monorepo.apollo-link-20200409212243

---
+++ src/queries/course.ts
@@ -0,0 +1,104 @@
+import gql from 'graphql-tag';
+
+export const GET_UNLOCKED_COURSES = gql`
+  query GetCourses {
+    unlockedCourses {
+      courseId
+      header
+      url
+      imageUrl
+      canUpgrade
+    }
+  }
+`;
+
+export const GET_UNLOCKED_COURSE = gql`
+  query GetCourse($courseId: CourseId!) {
+    unlockedCourse(courseId: $courseId) {
+      courseId
+      header
+      canUpgrade
+      introduction {
+        label
+        data {
+          label
+          description
+          url
+        }
+      }
+      onboarding {
+        label
+        data {
+          items {
+            label
+            description
+            url
+            secondaryUrl
+          }
+        }
+      }
+      bookDownload {
+        label
+        data {
+          items {
+            label
+            description
+            url
+            fileName
+          }
+        }
+      }
+      bookOnline {
+        label
+        data {
+          chapters {
+            label
+            url
+            sections {
+              label
+              url
+            }
+          }
+        }
+      }
+      curriculum {
+        label
+        data {
+          sections {
+            label
+            items {
+              kind
+              label
+              description
+              url
+              secondaryUrl
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_FREE_COURSE = gql`
+  mutation CreateFreeCourse(
+    $courseId: CourseId!
+    $bundleId: BundleId!
+  ) {
+    createFreeCourse(courseId: $courseId, bundleId: $bundleId)
+  }
+`;
+
+export const CREATE_ADMIN_COURSE = gql`
+  mutation CreateAdminCourse(
+    $uid: String!
+    $courseId: CourseId!
+    $bundleId: BundleId!
+  ) {
+    createAdminCourse(
+      uid: $uid
+      courseId: $courseId
+      bundleId: $bundleId
+    )
+  }
+`;
