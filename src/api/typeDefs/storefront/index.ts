76282a71edafd4eda15ac720ad3f6c3add2fa6c7
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:56:02 +0530

    Initialize greenkeeper/@sentry/node-5.14.2

---
+++ src/api/typeDefs/storefront/index.ts
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-server-micro';
+
+export default gql`
+  extend type Query {
+    storefrontCourse(
+      courseId: CourseId!
+      bundleId: BundleId!
+    ): StorefrontCourse
+
+    storefrontBundles(courseId: CourseId!): [Bundle!]!
+
+    storefrontCourses: [StorefrontCourse!]!
+  }
+
+  type StorefrontCourse {
+    header: String!
+    courseId: CourseId!
+    url: String!
+    imageUrl: String
+    bundle: Bundle!
+  }
+
+  type Bundle {
+    header: String!
+    bundleId: BundleId!
+    price: Int!
+    imageUrl: String!
+    benefits: [String!]!
+  }
+`;
