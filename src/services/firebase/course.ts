4b80222f9e25e81241006ef1797ea370a7b676a7
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:54:15 +0530

    Initialize greenkeeper/@graphql-codegen/typescript-resolvers-1.12.2

---
+++ src/services/firebase/course.ts
@@ -0,0 +1,36 @@
+import * as firebaseAdminVanilla from 'firebase-admin';
+
+import firebaseAdmin from '@services/firebase/admin';
+
+import { COURSE } from '../../../content/course-keys';
+import { BUNDLE } from '../../../content/course-keys';
+
+export const createCourse = async ({
+  uid,
+  courseId,
+  bundleId,
+  amount,
+  paymentType,
+}: {
+  uid?: string;
+  courseId: string;
+  bundleId: string;
+  amount: number;
+  paymentType: string;
+}) =>
+  await firebaseAdmin
+    .database()
+    .ref(`users/${uid}/courses`)
+    .push()
+    .set({
+      courseId: courseId,
+      packageId: bundleId,
+      invoice: {
+        createdAt:
+          firebaseAdminVanilla.database.ServerValue.TIMESTAMP,
+        amount,
+        licensesCount: 1,
+        currency: 'USD',
+        paymentType,
+      },
+    });
