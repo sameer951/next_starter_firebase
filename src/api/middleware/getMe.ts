ccd2dcf4e4020f604538a0f8dac3efeaf3049f15
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:55:32 +0530

    Initialize greenkeeper/monorepo.apollo-tooling-20200305190144

---
+++ src/api/middleware/getMe.ts
@@ -0,0 +1,38 @@
+import { AuthenticationError } from 'apollo-server-micro';
+import * as firebaseAdminVanilla from 'firebase-admin';
+
+import firebaseAdmin from '@services/firebase/admin';
+
+import { ServerResponse, ServerRequest } from '@typeDefs/server';
+
+type Claims = {
+  admin: boolean;
+};
+
+type AdminUser = {
+  customClaims: Claims;
+};
+
+export default async (req: ServerRequest, res: ServerResponse) => {
+  const { session } = req.cookies;
+
+  if (!session) {
+    return undefined;
+  }
+
+  const checkRevoked = true;
+
+  return await firebaseAdmin
+    .auth()
+    .verifySessionCookie(session, checkRevoked)
+    .then(async claims => {
+      return (await firebaseAdmin
+        .auth()
+        .getUser(
+          claims.uid
+        )) as firebaseAdminVanilla.auth.UserRecord & AdminUser;
+    })
+    .catch(error => {
+      throw new AuthenticationError(error.message);
+    });
+};
