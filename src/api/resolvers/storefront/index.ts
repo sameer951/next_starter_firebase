67b5c8fb9b2a3452e819486c3357155cab3951fd
Jyotikanta <sanjay.row.com@gmail.com>
2021-06-19 22:55:00 +0530

    Initialize greenkeeper/@sentry/node-5.12.5

---
+++ src/api/resolvers/storefront/index.ts
@@ -0,0 +1,24 @@
+import { QueryResolvers } from '@generated/server';
+
+import storefront from '@data/course-storefront';
+
+interface Resolvers {
+  Query: QueryResolvers;
+}
+
+export const resolvers: Resolvers = {
+  Query: {
+    storefrontCourse: (parent, { courseId, bundleId }) => {
+      const course = storefront[courseId];
+      const bundle = course.bundles[bundleId];
+
+      return {
+        ...course,
+        bundle,
+      };
+    },
+    storefrontCourses: () => {
+      return Object.values(storefront);
+    },
+  },
+};
